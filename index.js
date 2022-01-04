f28d317f6c6bc837586009073c8d08b4ecc07e0b
Krishhalde1 <96564042+Krishhalde1@users.noreply.github.com>
2022-01-04 16:10:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ var server = http.createServer(function(request, response) {
 var port = 80;
 server.listen(port);
 
-const port = 4000;
-app.listen(port, () => console.log("Backend : Server connected on port 4000"));
+const port1 = 4000;
+app.listen(port1, () => console.log("Backend : Server connected on port 4000"));

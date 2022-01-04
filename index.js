5d7f128c2a539792992c845f1999687aff07ef11
Krishhalde1 <krish16halde@gmail.com>
2022-01-04 10:44:12 +0530

    initial commit

--- index.js
+++ index.js
@@ -10,4 +10,5 @@ var server = http.createServer(function(request, response) {
 var port = 80;
 server.listen(port);
 
-console.log("Server running at http://localhost:%d", port);
+const port = 3000;
+app.listen(port, () => console.log("Backend : Server connected on port 3000"));
